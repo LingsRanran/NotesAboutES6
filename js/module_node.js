be951042063cc112659ad818e4a2d311290d5c7b
LingsRanran <1078421081@qq.com>
2018-06-25 11:15:32 +0800

    module in node

---
+++ js/module_node.js
@@ -0,0 +1,5 @@
+let str = '我是模块里的内容';
+
+
+console.log('module_node被加载了');
+module.exports.str = str;
---
+++ js/use_module_node.js
@@ -0,0 +1,4 @@
+const {str} = require('./module_node.js');
+
+console.log('我是模块的使用者')
+console.log(str);
